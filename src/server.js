dd863d1e0219074e2780ba93f7861d7daf6db771
Marcos Renan <devmarcos7@gmail.com>
2024-10-19 21:13:09 -0300

    criando primeiras rotas e testando respostas

--- src/server.js
+++ src/server.js
@@ -1,9 +1,36 @@
 import express from 'express'
 
+// criando servidor express
 const app = express();
+// recebendo como json
+app.use(express.json());
 
-app.get('/usuarios', (req, res) => {
-  res.send('Usuarios')
+//mock de dados com array
+const usuarios = [
+  { id: 1, nome: 'Juan', idade: 30 },
+  { id: 2, nome: 'María', idade: 25 },
+  { id: 3, nome: 'Pedro', idade: 35 },
+  { id: 4, nome: 'Ana', idade: 28 },
+  { id: 5, nome: 'Luis', idade: 40 },
+  { id: 6, nome: 'Laura', idade: 22 },
+  { id: 7, nome: 'Carlos', idade: 32 },
+  { id: 8, nome: 'Sofía', idade: 27 },
+  { id: 9, nome: 'Miguel', idade: 33 },
+  { id: 10, nome: 'Elena', idade: 29 }
+];
+
+//criando rota post
+app.post('/usuarios', (req, res) => {
+
+  usuarios.push(req.body)
+
+  res.status(201).json(req.body)
 });
+
+//criando rota get
+app.get('/usuarios', (req, res) => {
+  res.status(200).json(usuarios)
+});
+
 
 app.listen(3000);
