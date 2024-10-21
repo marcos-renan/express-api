0a36a456e16a444133c9d733a4e830a55aded146
Marcos Renan <devmarcos7@gmail.com>
2024-10-21 10:55:32 -0300

    Finalizando API com rostas put e delete

--- src/server.js
+++ src/server.js
@@ -5,35 +5,62 @@ const prisma = new PrismaClient();
 
 // criando servidor express
 const app = express();
+
 // recebendo como json
 app.use(express.json());
 
-//mock de dados com array
-const usuarios = [
-  { id: 1, nome: 'Juan', idade: 30 },
-  { id: 2, nome: 'María', idade: 25 },
-  { id: 3, nome: 'Pedro', idade: 35 },
-  { id: 4, nome: 'Ana', idade: 28 },
-  { id: 5, nome: 'Luis', idade: 40 },
-  { id: 6, nome: 'Laura', idade: 22 },
-  { id: 7, nome: 'Carlos', idade: 32 },
-  { id: 8, nome: 'Sofía', idade: 27 },
-  { id: 9, nome: 'Miguel', idade: 33 },
-  { id: 10, nome: 'Elena', idade: 29 }
-];
+//Rota post
+app.post('/usuarios', async (req, res) => {
 
-//criando rota post
-app.post('/usuarios', (req, res) => {
-
-  usuarios.push(req.body)
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age
+    }
+  })
 
   res.status(201).json(req.body)
+
 });
 
 //criando rota get
-app.get('/usuarios', (req, res) => {
-  res.status(200).json(usuarios)
+app.get('/usuarios', async(req, res) => {
+
+  const users = await prisma.user.findMany()
+
+  res.status(200).json(users)
 });
+
+app.put("/usuarios/:id", async (req, res) => {
+
+  await prisma.user.update({
+    where: {
+      id: req.params.id
+    },
+
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+
+    },
+  });
+
+  res.status(201).json(req.body);
+
+});
+
+app.delete('/usuarios/:id', async (req, res) => {
+
+  await prisma.user.delete({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  res.status(200).json({message: 'Usuário deletado com sucesso!'})
+})
 
 
 app.listen(3000);
